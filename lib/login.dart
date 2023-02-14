8bc74c0ed5dd87ae6ad6ca6f149ee950c43e7a21
tona <tonadaphine77@gmail.com>
2023-02-15 01:06:09 +0200

    login page

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -6,6 +7,81 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        color: Color.fromARGB(255, 35, 35, 35),
+        padding: const EdgeInsets.all(50.0),
+        child: Form(
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            const SizedBox(
+              height: 50,
+            ),
+            const Text(
+              'Login',
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            const Text(
+              'Enter your Phone number or Email address for sign in. Enjoy your food :)',
+              style: TextStyle(color: Colors.white),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                hintText: "jane@alustudent.com",
+                labelText: "Email",
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: "123password",
+                labelText: "Password",
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.lightGreen),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have an account?",
+                  style: TextStyle(color: Colors.white),
+                ),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Create One',
+                      style: TextStyle(color: Colors.lightGreen),
+                    ))
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:khana_app/onboarding.dart';
+import 'package:khana_app/login.dart';
 
 void main() => runApp(
     DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()));
@@ -18,10 +18,11 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       title: 'Registration Page',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: Colors.lightGreen,
+        colorScheme: ColorScheme.dark(),
       ),
       // direct the main page to the starting page
-      home: const OnboardingPage(),
+      home: const LoginPage(),
     );
   }
 }
